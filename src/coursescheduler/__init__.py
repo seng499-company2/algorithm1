0cf3df9b122d0a0289208f5bb92246d763a2284f
Shea Faigan <stfaigan@gmail.com>
2022-07-10 16:48:18 -0700

    Implemented schemas and validator functions

--- src/coursescheduler/__init__.py
+++ src/coursescheduler/__init__.py
@@ -1,1 +1,2 @@
 from .scheduler import generate_schedule
+from .models import validate_schedule, validate_professor, validate_professors
--- src/coursescheduler/models.py
+++ src/coursescheduler/models.py
@@ -1,59 +1,109 @@
-# This file contains a set of example data models to be used within the algorithm.
-# This file will eventually convert the input data into the structures below.
+import re
+from schema import Schema, Or, And
 
-# A dictionary of courses where the course codes are the keys and the values are the attributes of each course
-courses = {
-    "course code1": {
-        "pengRequired": "boolean",
-        "yearRequired": "int",
-        "semester": "string",
-        "professor": "professor object",
-        "timeSlots": "list of timeSlot objects",
-        "academicYear": "int"
+twentyFourHourTimeRegex = r'^((0?|1)[0-9]|2[0-3]):[0-5][0-9]$'
+
+
+def validate_time_ranges(time_ranges):
+    assert len(time_ranges) == 2
+    assert re.match(twentyFourHourTimeRegex, time_ranges[0])
+    assert re.match(twentyFourHourTimeRegex, time_ranges[1])
+    assert int(time_ranges[0].replace(":", "")) < int(time_ranges[1].replace(":", ""))
+    return True
+
+
+CoursePreference = Schema({
+    "courseCode": str,
+    "enthusiasmScore": int,
+})
+
+TimeRange = Schema(And(Or(list, tuple), validate_time_ranges))
+
+DayTimes = Schema({
+    "monday": [TimeRange],
+    "tuesday": [TimeRange],
+    "wednesday": [TimeRange],
+    "thursday": [TimeRange],
+    "friday": [TimeRange],
+})
+
+Professor = Schema({
+    "id": str,
+    "name": str,
+    "isPeng": bool,
+    "facultyType": Or("RESEARCH", "TEACHING"),
+    "coursePreferences": [CoursePreference],
+    "teachingObligations": int,
+    "preferredTimes": {
+        "fall": Or(None, DayTimes),
+        "spring": Or(None, DayTimes),
+        "summer": Or(None, DayTimes),
     },
-    "course code2": {
-        "pengRequired": "boolean",
-        "yearRequired": "int",
-        "semester": "string",
-        "professor": "professor object",
-        "timeSlots": "list of timeSlot objects",
-        "academicYear": "int"
+    "preferredCoursesPerSemester": {
+        "fall": And(int, lambda n: n >= 0),
+        "spring": And(int, lambda n: n >= 0),
+        "summer": And(int, lambda n: n >= 0),
     },
-}
+    "preferredNonTeachingSemester": Or("FALL", "SPRING", "SUMMER", None),
+    "preferredCourseDaySpreads": ["TWF", "MTh", "M", "T", "W", "Th", "F"],
+})
 
-# A dictionary of professors where the professor ids are the keys and the values are the attributes of each professor
-professors = {
-    "id": {
-        "name": "string",
-        "isPeng": "boolean",
-        "facultyType": "string",
-        "coursePreferences": "list of coursePreference objects",
-        "teachingObligations": "int",
-        "preferredTimes": "dictionary of DayTimes objects",
-        "preferredCoursesPerSemester": "dictionary of fall, spring, and summer ints",
-        "preferredNonTeachingSemester": "string",
-        "preferredCourseDaySpreads": "a course day spread object"
+Course = Schema({
+    "code": str,
+    "title": str,
+    "pengRequired": {
+        "fall": bool,
+        "spring": bool,
+        "summer": bool,
     },
-}
+    "yearRequired": Or(1, 2, 3, 4),
+})
 
-# Basic example of the time slot configuration.
-# Below is a possible solution change if the time slot dictionary is too slow
-# Conflict determination: IF list is same length, only need to compare 1 value
-# First check if the day overlaps, then check if time overlaps
-# MR and TWF will never need to be compared
-timeslots = {
-    1: [["TUESDAY", "830", "920"], ["WEDNESDAY", "830", "920"], ["FRIDAY", "830", "920"]],
-    2: [["TUESDAY", "900", "950"], ["WEDNESDAY", "900", "950"], ["FRIDAY", "900", "950"]],
-    3: [["MONDAY", "900", "1020"], ["THURSDAY", "900", "1020"]],
-    4: [["TUESDAY", "900", "1150"]]
-}
+TimeSlot = Schema({
+    "dayOfWeek": Or("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"),
+    "timeRange": TimeRange,
+})
+
+ProfessorSlim = Schema({
+    "id": str,
+    "name": str,
+})
+
+CourseSection = Schema({
+    "professor": Or(None, ProfessorSlim),
+    "capacity": Or(None, int),
+    "timeSlots": And([TimeSlot], lambda x: len(x) in [0, 1, 2, 3])  # And(list, validate_time_slots),
+})
+
+CourseOffering = Schema({
+    "course": Course,
+    "sections": And([CourseSection], lambda x: len(x) in [0, 1, 2]),  # And(list, validate_course_sections),
+})
+
+Schedule = Schema({
+    "fall": [CourseOffering],
+    "spring": [CourseOffering],
+    "summer": [CourseOffering],
+})
 
 
-# conflicts = {
-#     1: ABC,
-#     2: BC
-# }
-# set_1.isdisjoint(set_2)
-# domain = [1, 2, 3, 4, 5, . . . . . ]
+def validate_schedule(schedule, print_output=True):
+    Schedule.validate(schedule)
+    if print_output:
+        print("Schedule adheres to specification")
+    return True
 
-# time_conflicts = {(id1, id2) : bool does_conflict}
+
+def validate_professor(professor, print_output=True):
+    Professor.validate(professor)
+    if print_output:
+        print("Professor adheres to specification")
+    return True
+
+
+def validate_professors(professors, print_output=True):
+    for professor in professors:
+        validate_professor(professor, print_output=False)
+    if print_output:
+        print("All professors adhere to specification")
+    return True
