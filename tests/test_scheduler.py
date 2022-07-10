e18d1f1f7e2c1c75eaa942b06560744530c39f4b
Shea Faigan <stfaigan@gmail.com>
2022-07-10 16:49:41 -0700

    Implemented test to validate scheduler output meets spec

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,8 +1,11 @@
 from unittest import TestCase
+from src.coursescheduler import generate_schedule
+from src.coursescheduler.models import Schedule
 
 class PyTestTesting(TestCase):
     def test_always_passes(self):
         self.assertTrue(True)
 
-    def test_always_fails(self):
-        self.assertTrue(True)
+    def test_scheduler_output_meets_spec(self):
+        schedule = generate_schedule(None, None, True)
+        Schedule.validate(schedule)
